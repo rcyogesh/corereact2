7091c020fa025a2853a5fc1a43616f1da0117fe6
Pradip Chandratre <rcyogesh@gmail.com>
2021-12-02 23:08:46 -0500

    wtf state

--- ClientApp/src/components/wtf.js
+++ ClientApp/src/components/wtf.js
@@ -1,6 +1,27 @@
 ﻿import React, { Component } from 'react';
 export class wtf extends Component {
+
+    
+    constructor(props) {
+        super(props);
+        this.state = { i : 0 };
+    }
+
+    componentWillMount() {
+        //setTimeout(()=> this.increment(this), 500);
+        setInterval(() => this.increment(), 500);
+    }
+
+    //increment(w) {
+    //    w.setState({ i:w.state.i + 1 });
+    //    setTimeout(()=>w.increment(w), 500);
+    //}
+
+    increment() {
+        this.setState({ i: this.state.i + 1 });
+    }
+
     render() {
-        return (<h1>who taa foo</h1>);
+        return (<h1>who taa foo {this.state.i}</h1>);
     }
 }
